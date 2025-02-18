f0ce6b7dff4ead9a67062f151c9e4f285bd98920
212324 <212324@WINMAC7>
2025-02-18 14:00:54 +0300

    переделал 6 практику

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,95 +1,187 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
+using System.Diagnostics;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
+using System.Net.Http.Headers;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
-namespace Calcul
+using System.Xml;
+
+namespace практика6
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Введите А");
-            int a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите Б");
-            int b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Выберите действие над числами + (1), -(2), /(3), *(4)");
-            int c = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Ответ: " + Calcul(a, b, c));
-            Console.WriteLine("Максимальное число: " + Max(a, b));
-            Console.WriteLine("Минимальное число: " + Min(a, b));
-            Console.WriteLine("Введите число для нахождения его факториала n ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Факториал числа  {n} равен: {Factorial(n)} ");
-            Console.WriteLine("Введите первое число для нахождения среднего");
-            int z = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите второе число для нахождения среднего");
-            int x = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите третье число для нахождения среднего");
-            int v = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Среднее число от чисел {z}, {x}, {v} равняется: {Average(z, x, v)}");
+        {;
+            Console.WriteLine("1 task");
+
+            Console.Write("Введите a: "); int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите b: "); int b = int.Parse(Console.ReadLine());
+            Calcul(a, b);
+
+            Console.WriteLine("2 task");
+            Console.Write("Введите a: "); int p = int.Parse(Console.ReadLine());
+            Console.Write("Введите b: "); int o = int.Parse(Console.ReadLine());
+            Min(p, o);
+            Max(p, o);
+
+            Console.WriteLine("3 task");
+
+            Console.Write("Введите строку либо число для проверки: "); string str = Console.ReadLine();
+            IsPalindrome(str);
+
+            Console.WriteLine("4 task");
+
+            Console.Write("Введите n: "); int n = int.Parse(Console.ReadLine());
+            Factorial(n);
+
+            Console.WriteLine("5 task");
+
+            Console.Write("Введите a: "); int m = int.Parse(Console.ReadLine());
+            Console.Write("Введите b: "); int v = int.Parse(Console.ReadLine());
+            Console.Write("Введите c: "); int d = int.Parse(Console.ReadLine());
+            Average(m, v, d);
+
+            Console.WriteLine("6 task");
+
+            Console.Write("Введите слово или текст: "); string text = Console.ReadLine();
+            Console.Write("Введите символ для поиска: "); string symb = Console.ReadLine();
+            FindChar(text, symb);
+
+            Console.WriteLine("7 task");
+
+            Console.Write("Введите длину пароля: "); int length = int.Parse(Console.ReadLine());
+            GeneratePassword(length);
+
+            Console.WriteLine("8 task");
+
+            Console.Write("Введите градус в Цельсиях: "); double c = double.Parse(Console.ReadLine());
+            Console.Write("Введите градус в Фаренгейтах: "); double f = double.Parse(Console.ReadLine());
+            CelsiusToFahrenheit(c);
+            FahrenheitToCelsius(f);
+
+            Console.WriteLine("9 task");
+
+            Console.Write("Введите предложение для перестановки: "); string sentence = Console.ReadLine();
+            ReverseWords(sentence);
+
+            Console.WriteLine("10 task");
+
+            Console.Write("Введите число: "); int w = int.Parse(Console.ReadLine());
+            MultiplicationTable(w);
+
             Console.ReadKey();
         }
-        public static string Calcul(int a, int b, int c)
+        public static void Calcul(int a, int b)
         {
-            if (c == 1)
-            {
-                a = a + b;
-            }
-            if (c == 2)
-            {
-                a = a - b;
-            }
-            if (c == 3)
-            {
-                a = a / b;
-            }
-            if (c == 4)
-            {
-                a = a * b;
-            }
-            return a.ToString();
-        }
-        public static int Max(int a, int b)
-        {
-            if (a < b)
-            {
-                return b;
-            }
-            else
-            {
-                return a;
-            }
-        }
-        public static int Min(int a, int b)
-        {
-            if (a > b)
-            {
-                return b;
-            }
-            else
-            {
-                return a;
-            }
-        }
-        public static int Factorial(int n)
-        {
-            int factorial = 1;
-            int i = 1;
-            while (i <= n)
-            {
-                factorial = factorial * i;
-                i++;
-            }
+            int chas = a / b;
+            Console.WriteLine($"Частное чисел {a} и {b} = {chas}");
+            int proiz = a * b;
+            Console.WriteLine($"Произведение чисел {a} и {b} = {proiz}");
+            int raz = a - b;
+            Console.WriteLine($"Разность чисел {a} и {b} = {raz}");
+            int sum = a + b;
+            Console.WriteLine($"Сумма чисел {a} и {b} = {sum}");
 
-            return factorial;
         }
-        public static int Average(int z, int x, int v)
+        public static void Min(int p, int o)
         {
-            int average = (z + x + v) / 3;
-            return average;
+            if (p < o)
+            {
+                Console.WriteLine($"Наименьшее число - {p}");
+            }
+            else
+            {
+                Console.WriteLine($"Наименшьее число - {o}");
+            }
+        }
+        public static void Max(int p, int o)
+        {
+            if (p > o)
+            {
+                Console.WriteLine($"Наибольшее число - {p}");
+            }
+            else
+            {
+                Console.WriteLine($"Наибольшее число - {o}");
+            }
+        }
+        public static void IsPalindrome(string str)
+        {
+            var revstr = new string(str.Reverse().ToArray());
+            if (revstr == str)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+        public static void Factorial(int n)
+        {
+            var factorial = 1;
+            for (int i = 1; i <= n; i++)
+                factorial *= i;
+            Console.WriteLine($"Факториал числа {n} = {factorial}");
+        }
+        public static void Average(int m, int v, int d)
+        {
+            int aver = (m + v + d) / 3;
+            Console.WriteLine($"Среднее арифмитическое чисел {m}, {v}, {d} = {aver}");
+        }
+        public static void FindChar(string text, string symb)
+        {
+            int index = text.IndexOf(symb);
+
+            if (index != -1)
+            {
+                Console.WriteLine(index);
+            }
+            else
+            {
+                Console.WriteLine($"-1");
+            }
+        }
+        public static void GeneratePassword(int length)
+        {
+            string password = "";
+            Random random = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int gap = random.Next(0, 9);
+                password += gap.ToString();
+            }
+            Console.WriteLine($"Ваш пароль: {password}");
+
+        }
+        public static void CelsiusToFahrenheit(double c)
+        {
+            int F = Convert.ToInt32(c) * (9 / 5) + 32;
+            Console.WriteLine($"{c} градусов Цельсия в Фаренгейтах будет - {F}");
+        }
+        public static void FahrenheitToCelsius(double f)
+        {
+            int C = (Convert.ToInt32(f) - 32) * (5 / 9);
+            Console.WriteLine($"{f} градусов Цельсия в Фаренгейтах будет - {C}");
+        }
+        public static void ReverseWords(string sentence)
+        {
+            string[] words = sentence.Split(' ');
+            Array.Reverse(words);
+            Console.Write("Перестановленное предложение: ");
+            Console.WriteLine(string.Join(" ", words));
+        }
+        public static void MultiplicationTable(int w)
+        {
+            Console.WriteLine($"Таблица умножения на {w}");
+            for (int i = 1; i <= 10; i++)
+            {
+
+                Console.WriteLine($"{w} x {i} = {w * i}");
+            }
         }
     }
 }
